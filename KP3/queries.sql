6c069007304b2cdbe8047c61335b5d1618ee47fe
Oleh Pedorenko <Oleh_Pedorenko@epam.com>
2016-05-26 19:07:18 +0300

    Added KP3 tasks

--- KP3/queries.sql
+++ KP3/queries.sql
@@ -61,3 +61,67 @@ FROM
     INNER JOIN product USING(id_product)
     INNER JOIN product_type USING(id_product_type)))
 WHERE dnr = 1;
+
+-- 4
+SELECT DISTINCT
+  id_product,
+  price,
+  ROUND((MAX(date_oper) OVER (PARTITION BY id_product)) 
+        - (MIN(date_oper) OVER (PARTITION BY id_product)), 0) AS storage_period
+FROM store
+INNER JOIN product USING(id_product)
+ORDER BY storage_period*price DESC;
+
+-- 5a
+SELECT 
+  id_product,
+  product_name,
+  id_invoice,
+  LAG(id_invoice, 1, -1) OVER (PARTITION BY id_product ORDER BY purchase_time ASC) AS prev_invoice,
+  purchase_time,
+  LAG(purchase_time, 1, NULL) OVER (PARTITION BY id_product ORDER BY purchase_time ASC) AS prev_purchase_time
+FROM invoice
+INNER JOIN invoice_detail USING(id_invoice)
+INNER JOIN product USING(id_product);
+
+-- 5b
+SELECT 
+  id_product,
+  product_name,
+  id_invoice,
+  LEAD(id_invoice, 1, -1) OVER (PARTITION BY id_product ORDER BY purchase_time DESC) AS prev_invoice,
+  purchase_time,
+  LEAD(purchase_time, 1, NULL) OVER (PARTITION BY id_product ORDER BY purchase_time DESC) AS prev_purchase_time
+FROM invoice
+INNER JOIN invoice_detail USING(id_invoice)
+INNER JOIN product USING(id_product);
+
+-- 6a
+SELECT 
+  product_type_name,
+  product_name,
+  price,
+  FIRST_VALUE(product_name) OVER(PARTITION BY id_product_type ORDER BY price) AS cheapest_product,
+  FIRST_VALUE(price) OVER(PARTITION BY id_product_type ORDER BY price) AS cheapest_price
+FROM product
+INNER JOIN product_type USING(id_product_type);
+
+-- 6b
+SELECT 
+  product_type_name,
+  product_name,
+  price,
+  MIN(price) OVER(PARTITION BY id_product_type) AS cheapest_type_price
+FROM product
+INNER JOIN product_type USING(id_product_type);
+
+-- 6c
+SELECT
+  id_stuff,
+  name,
+  surname,
+  id_position,
+  position_name,
+  COUNT(*) OVER (PARTITION BY id_position) AS count_colleagues  
+FROM stuff
+INNER JOIN position USING (id_position);
