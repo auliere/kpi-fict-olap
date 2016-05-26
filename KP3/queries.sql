c65b3d3984f07bf1dde4d5707ebe1568154af79c
Oleh Pedorenko <Oleh_Pedorenko@epam.com>
2016-05-26 17:31:57 +0300

    KP3 task 3

--- KP3/queries.sql
+++ KP3/queries.sql
@@ -38,3 +38,26 @@ FROM
   FROM product
   INNER JOIN product_type USING (id_product_type))
 WHERE R <=2;
+
+-- 3
+SELECT 
+  product_type_name,
+  top_product,
+  sold_total
+FROM
+  (SELECT 
+    product_type_name,
+    product_name AS top_product,
+    sold_total,
+    DENSE_RANK() OVER (PARTITION BY id_product_type ORDER BY sold_total DESC) AS dnr
+  FROM
+    (SELECT DISTINCT
+      id_product,
+      id_product_type,
+      product_name,
+      product_type_name,
+      SUM(quantity) OVER (PARTITION BY id_product) AS sold_total  
+    FROM invoice_detail
+    INNER JOIN product USING(id_product)
+    INNER JOIN product_type USING(id_product_type)))
+WHERE dnr = 1;
